d8823da501b965c96b6c47d07386c5603ceabf12
github username <github email address>
2017-11-29 09:56:53 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    return_collect = []
+    
+    while i < collection.size
+        if yield(collection[i]) == true
+        return_collect.push(collection[i])
+    end
+    i += 1
+    end
+    return_collect
 end
