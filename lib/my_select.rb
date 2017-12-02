abd7aa91dd80e5ea6b1d37682ec692511be88749
github username <github email address>
2017-12-02 10:10:39 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+# what is the return value of yielding a character to a block
+
+# Do whatever you want with the element (yield) but I'm just going to check the return value of the block for truthiness or falsiness.
+def my_select(arr)
+counter = 0
+results = []
+while counter < arr.length
+  if yield(arr[counter]) == true
+    results.push(arr[counter])
+  end
+  counter += 1
+end
+results
 end
